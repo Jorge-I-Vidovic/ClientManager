490903970f50157c24ce47e8e5a3772471dce30b
Jorge I. Vidovic <jorgeisaacvidovich@gmail.com>
2023-06-09 18:19:09 +0200

    First Commmit

---
+++ src/main/java/ClientManager/ClientManagerApp.java
@@ -0,0 +1,13 @@
+package ClientManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientManagerApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientManagerApp.class, args);
+    }
+
+}
