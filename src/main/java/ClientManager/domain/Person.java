e2b0807898891f7bf53d71956736d95b0455e79c
Jorge I. Vidovic <jorgeisaacvidovich@gmail.com>
2023-06-15 15:06:42 +0200

    Adding validations and templates to the project.

--- src/main/java/ClientManager/domain/Person.java
+++ src/main/java/ClientManager/domain/Person.java
@@ -1,20 +1,29 @@
 package ClientManager.domain;
+
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
+import java.io.Serial;
 import java.io.Serializable;
 
 @Data
 @Entity
 @Table(name = "persona")
 public class Person implements Serializable {
+    @Serial
     private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id_persona;
+
+    @NotEmpty
     private String nombre;
+    @NotEmpty
     private String apellido;
+    @NotEmpty
     private String email;
+    @NotEmpty
     private String telefono;
 }
--- src/main/java/ClientManager/web/ControladorInicio.java
+++ src/main/java/ClientManager/web/ControladorInicio.java
@@ -2,10 +2,12 @@ package ClientManager.web;
 
 import ClientManager.domain.Person;
 import ClientManager.service.PersonService;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -29,7 +31,10 @@ public class ControladorInicio {
     }
 
     @PostMapping("/savePerson")
-    public String savePerson(Person person) {
+    public String savePerson(@Valid Person person, Errors er) {
+        if(er.hasErrors()){
+            return "modifyPerson";
+        }
         personServ.savePerson(person);
         return "redirect:/";
     }
