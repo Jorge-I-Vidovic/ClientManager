b49588b2270bf2cc3a17fe261a2b396ace033ff5
Jorge I. Vidovic <jorgeisaacvidovich@gmail.com>
2023-06-24 21:01:37 +0200

    Adding security to the project. Updating.

--- src/main/java/ClientManager/web/SecurityConfig.java
+++ src/main/java/ClientManager/web/SecurityConfig.java
@@ -2,6 +2,7 @@ package ClientManager.web;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -9,10 +10,22 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .httpBasic()
+                .and().authorizeHttpRequests()
+                .requestMatchers("/").hasRole("USER")
+                .requestMatchers(HttpMethod.POST, "/addPerson").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.GET, "/addPerson").authenticated()
+                .and().csrf().disable().build();
+    }
+
     @Bean
     public UserDetailsService userDetailsService() {
         return new InMemoryUserDetailsManager(
